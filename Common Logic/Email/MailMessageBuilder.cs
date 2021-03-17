4bf19d58d89e2a11ad274f13a8c0da75681cc1a9
Jake H <jakehumphreys@outlook.com>
2021-03-17 21:27:33 +0000

    Added email builders

---
+++ Common Logic/Email/MailMessageBuilder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using WebGrease.Css;
+
+namespace FYP_WebApp.Common_Logic.Email
+{
+    public class MailMessageBuilder
+    {
+        private readonly MailMessage _mailMessage;
+
+        public MailMessageBuilder()
+        {
+            _mailMessage = new MailMessage();
+        }
+
+        public MailMessageBuilder To(MailAddress address)
+        {
+            _mailMessage.To.Add(address);
+            return this;
+        }
+
+        public MailMessageBuilder Cc(MailAddress address)
+        {
+            _mailMessage.CC.Add(address);
+            return this;
+        }
+
+        public MailMessageBuilder Bcc(MailAddress address)
+        {
+            _mailMessage.Bcc.Add(address);
+            return this;
+        }
+
+        public MailMessageBuilder From(MailAddress address)
+        {
+            _mailMessage.Sender = address;
+            return this;
+        }
+
+        public MailMessageBuilder Subject(string subject)
+        {
+            _mailMessage.Subject = subject;
+            return this;
+        }
+
+        public MailMessageBuilder Body(string body)
+        {
+            _mailMessage.Body = body;
+            return this;
+        }
+
+        public MailMessageBuilder IsBodyHtml(bool isBodyHtml)
+        {
+            _mailMessage.IsBodyHtml = isBodyHtml;
+            return this;
+        }
+
+        public MailMessage Build()
+        {
+            return _mailMessage;
+        }
+    }
+}
---
+++ Common Logic/Email/SmtpClientBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using Microsoft.Owin.Security.Facebook;
+
+namespace FYP_WebApp.Common_Logic.Email
+{
+    public class SmtpClientBuilder
+    {
+        private readonly SmtpClient _smtpClient;
+
+        public SmtpClientBuilder()
+        {
+            _smtpClient = new SmtpClient();
+        }
+
+        public SmtpClientBuilder Host(string hostAddress)
+        {
+            _smtpClient.Host = hostAddress;
+            return this;
+        }
+
+        public SmtpClientBuilder Port(int port)
+        {
+            _smtpClient.Port = port;
+            return this;
+        }
+
+        public SmtpClientBuilder Credentials(NetworkCredential credentials)
+        {
+            _smtpClient.Credentials = credentials;
+            return this;
+        }
+
+        public SmtpClientBuilder EnableSsl(bool isEnabled)
+        {
+            _smtpClient.EnableSsl = isEnabled;
+            return this;
+        }
+
+        public SmtpClient Build()
+        {
+            return _smtpClient;
+        }
+    }
+}
